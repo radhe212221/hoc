568734533b7f59df4581b098ec7bc232b4bc2422
radhe212221 <radhe212221@gmail.com>
2022-05-26 21:29:44 +0530

    hoc completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { useState } from 'react';
 import './style.css';
 
+const C1 = (props) => {
+  return <h1 onClick={props?.onchilClick}>counter : {props?.x}</h1>;
+};
+
+const C2 = (props) => {
+  return <h2 onMouseOver={props?.onchilClick}>counter : {props?.x}</h2>;
+};
+
+const C3 = (props) => {
+  return <h3 onDoubleClick={props?.onchilClick}>counter : {props?.x}</h3>;
+};
+function hoc(Comp) {
+  const Feature = (props) => {
+    const [x, setx] = useState(0);
+    return <Comp x={x} onchilClick={() => setx(x + 1)} />;
+  };
+  return <Feature />;
+}
+
 export default function App() {
   return (
     <div>
-      <C1 />
-      <C2 />
-      <C3 />
+      {hoc(C1)}
+      {hoc(C2)}
+      {hoc(C3)}
     </div>
   );
 }
-
-const C1 = () => {
-  const [x, setx] = useState(0);
-  return <h1 onClick={() => setx(x + 1)}>counter : {x}</h1>;
-};
-
-const C2 = () => {
-  const [x, setx] = useState(0);
-  return <h2 onMouseOver={() => setx(x + 1)}>counter : {x}</h2>;
-};
-
-const C3 = () => {
-  const [x, setx] = useState(0);
-  return <h3 onDoubleClick={() => setx(x + 1)}>counter : {x}</h3>;
-};

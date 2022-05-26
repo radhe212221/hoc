a8b6353fc88c4f049696355d40badc990583eb27
radhe212221 <radhe212221@gmail.com>
2022-05-26 21:09:02 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-o69qmj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-o69qmj)
---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <C1 />
+      <C2 />
+      <C3 />
+    </div>
+  );
+}
+
+const C1 = () => {
+  const [x, setx] = useState(0);
+  return <h1 onClick={() => setx(x + 1)}>counter : {x}</h1>;
+};
+
+const C2 = () => {
+  const [x, setx] = useState(0);
+  return <h2 onMouseOver={() => setx(x + 1)}>counter : {x}</h2>;
+};
+
+const C3 = () => {
+  const [x, setx] = useState(0);
+  return <h3 onDoubleClick={() => setx(x + 1)}>counter : {x}</h3>;
+};
